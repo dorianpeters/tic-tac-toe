8b7d6b1a9b13861349d00aa75f0bdf0049fa8804
Dorian Peters <dpeters08@gmail.com>
2024-04-21 16:49:04 -0700

    Create Gameboard IIFE

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Tic-Tac-Toe");
+const Gameboard = (function () {
+  const board = [];
+  const size = 9; // 3 x 3 board
+  board.length = size; // give board array size of 9
+  // board.fill(null);
+  return { board };
+})();
+
+console.log(Gameboard.board);
