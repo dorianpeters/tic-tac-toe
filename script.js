773d59a9cbc9149f48cfe2fd42ed8475ce029b86
Dorian Peters <dpeters08@gmail.com>
2025-03-26 17:46:20 -0700

    Add note, make board blank

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ const Game = (function () {
     // const player1name = prompt("What is Player 1's name?");
     // const player2name = prompt("What is Player 2's name?");
     const player1name = "Dorian";
-    const player2name = "Farrah";
+    const player2name = "Fatima";
     const player1 = createPlayer(player1name, "X");
     const player2 = createPlayer(player2name, "O");
-    console.log(player1.name);
-    console.log(player2.name);
+    console.log(player1.name, player1.symbol);
+    console.log(player2.name, player2.symbol);
     Gameboard.displayBoard();
+    // Draft turn function that alternates players and checks for winner
   };
   return { game };
 })();
 
 const Gameboard = (function () {
   const board = [
-    ["#", "#", "#"],
-    ["#", "#", "#"],
-    ["#", "#", "#"],
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
   ];
   const displayBoard = () => {
     console.table(board);
@@ -28,7 +29,7 @@ const Gameboard = (function () {
 const createPlayer = function (name, symbol) {
   this.name = name;
   this.symbol = symbol;
-  return { name };
+  return { name, symbol };
 };
 
 Game.game();
